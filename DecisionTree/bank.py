785de7489fbbcc068ebe0512a3ebf982bdd36d52
Omid <omid.asudeh@gmail.com>
2021-09-24 23:29:04 -0600

    final changes

--- DecisionTree/bank.py
+++ DecisionTree/bank.py
@@ -24,14 +24,26 @@ def convert_numerical_to_binary(df, Label_col_index):
             df[col_idx] = pd.to_numeric(df[col_idx], errors='coerce')
             threashold = df[col_idx].median()
             print("preprocessing numerical attribute:", attrib_name[col_idx], "threashold:", threashold)
-            df[col_idx] = df[col_idx].apply(lambda x: "0" if int(x) < threashold else "1") 
-
+            df[col_idx] = df[col_idx].apply(lambda x: "0" if int(x) < threashold else "1")
 ######## preprocess the data
-print("########### preprocessing the data ##########")
+######## convert the numerical to binary
 train_df_processed = train_df
 test_df_processed = test_df
 convert_numerical_to_binary(train_df_processed, 16)
 convert_numerical_to_binary(test_df_processed, 16)
+
+def replace_unknown_with_mode(df, Label_col_index):
+    df.replace({"unknown":np.nan}, inplace=True)
+    for col_idx in range(Label_col_index):
+        if attrib_name[col_idx] in categorical_attrib_values:
+            mode = df.groupby(col_idx)[col_idx].count().idxmax()
+            df[col_idx].fillna(mode, inplace=True)
+
+######## set the unknown to mode
+train_df_processed_unknown_set_to_mode = train_df_processed
+test_df_processed_unknown_set_to_mode = test_df_processed
+replace_unknown_with_mode(train_df_processed_unknown_set_to_mode, 16)
+replace_unknown_with_mode(test_df_processed_unknown_set_to_mode, 16)
 
 # calculates the information gain
 def entropy_gain(S,Label_col_index, attrib_idx):
@@ -176,7 +188,7 @@ print(tree_gini)
 print("train accuracy:",predict_dataset(train_df_processed, tree_gini,16))
 
 
-print("\n\n########## experiment #############")
+print("\n\n########## experiment 1 #############")
 print("this may take few minutes dependeng to you machine (for me it took less than 5 mins to complete)")
 for hight in range(1,17):
     print("tree hight:", hight)
@@ -191,3 +203,17 @@ for hight in range(1,17):
         print("test accuracy: ", predict_dataset(test_df_processed, tree,16))
         print("###############")
 
+print("\n\n########## experiment 2 #############")
+print("this replaces the 'unknown' in categorical attributes with the column mode")
+for hight in range(1,17):
+    print("tree hight:", hight)
+    for app in ["EN", "GI", "ME"]:
+        print("gain approach:", app)
+        print("Training ...")
+        Attributes = [0,1,2,3,4,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] # initially put all attributes except the label in Attributes set
+        tree = ID3(train_df_processed_unknown_set_to_mode, Attributes,16, hight, app)
+        
+        print("prediction ...")
+        print("train accuracy:",predict_dataset(train_df_processed_unknown_set_to_mode, tree,16))
+        print("test accuracy: ", predict_dataset(test_df_processed_unknown_set_to_mode, tree,16))
+        print("###############")
